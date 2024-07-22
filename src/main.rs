9d8ec8d4738cd35a5befea1a23f4deadc9df4443
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-07-23 08:01:41 +1000

    add error colours

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use args::cli::{self, Args};
-use error::DeoProfileError;
-use profiles::ProfileConfig;
+use args::cli;
 
 mod args;
 mod entry_type;
@@ -13,30 +11,8 @@ mod profiles;
 mod error;
 mod debug;
 mod colours;
+mod workflow;
 
 fn main() {
-  let args = cli::get_cli_args();
-  match profiles::read_profile_config() {
-    Ok(profile_config) => encode_profiles(args, profile_config),
-    Err(error) => eprintln!("{}", error)
-  }
-}
-
-fn encode_profiles(args: Args, profile_config: ProfileConfig) {
-  let sessions_to_encode_dir = file_mapper::get_session_encode_mapping(args.source, args.verbose);
-  if sessions_to_encode_dir.is_empty() {
-    println!("Could not find any renames to encode")
-  } else {
-    match user_choices::interact_with_user(sessions_to_encode_dir, profile_config) {
-      user_choices::Interaction::ProceedToEncode(selections) => {
-        match handbrake::encode(selections) {
-          Ok(_) => (),
-          Err(error) => eprintln!("{}", error),
-        }
-      },
-      user_choices::Interaction::NoFilesToEncode => eprintln!("No files to encode"),
-      user_choices::Interaction::CancelEncode => println!("User canceled encoding"),
-      user_choices::Interaction::InteractionError(error) => eprintln!("Interaction with the user raised an error: {}", error),
-    }
-  }
+  workflow::perform(cli::get_cli_args())
 }
---
+++ src/workflow.rs
@@ -0,0 +1,33 @@
+use crate::args::cli::Args;
+use crate::{file_mapper, handbrake, user_choices};
+use crate::profiles::{self, ProfileConfig};
+use crate::colours;
+use console::style;
+
+
+pub fn perform(args: Args) {
+  match profiles::read_profile_config() {
+    Ok(profile_config) => encode_profiles(args, profile_config),
+    Err(error) => eprintln!("{}", style(error).bg(colours::RED))
+  }
+}
+
+
+fn encode_profiles(args: Args, profile_config: ProfileConfig) {
+  let sessions_to_encode_dir = file_mapper::get_session_encode_mapping(args.source, args.verbose);
+  if sessions_to_encode_dir.is_empty() {
+    println!("Could not find any renames to encode")
+  } else {
+    match user_choices::interact_with_user(sessions_to_encode_dir, profile_config) {
+      user_choices::Interaction::ProceedToEncode(selections) => {
+        match handbrake::encode(selections) {
+          Ok(_) => (),
+          Err(error) => eprintln!("{}", style(error).bg(colours::RED)),
+        }
+      },
+      user_choices::Interaction::NoFilesToEncode => eprintln!("{}", style("No files to encode").bg(colours::RED)),
+      user_choices::Interaction::CancelEncode => println!("User cancelled encoding"),
+      user_choices::Interaction::InteractionError(error) => eprintln!("{}", style(format!("Interaction with the user raised an error: {}", error)).bg(colours::RED)),
+    }
+  }
+}
