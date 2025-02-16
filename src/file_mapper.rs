e03a7b649aa71a2061a29e0c8b44dd8916680075
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2025-02-17 06:20:55 +1100

    add regexes for movie matches

--- src/file_mapper.rs
+++ src/file_mapper.rs
@@ -6,9 +6,11 @@ use crate::debug::*;
 use std::sync::LazyLock;
 use crate::entry_type::{EncodeDir, EntryType, RenameFile, Session, SessionId, SessionToEncodeDir};
 
-static RENAME_FILE_REG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(session\d{1,})\/renames\/((S\d{2,}E\d{2,})\s-\s(.+.mkv))$").unwrap());
+static RENAME_TV_SERIES_FILE_REG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(session\d{1,})\/renames\/((S\d{2,}E\d{2,})\s-\s(.+.mkv))$").unwrap());
+static RENAME_MOVIE_FILE_REG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(session\d{1,})\/renames\/(.+.mkv)$").unwrap());
 static ENCODE_FILE_REG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(session\d{1,})\/renames\/encode_dir\.txt$").unwrap());
-static ENCODE_DIR_REG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r".+\/(.+\s\{tvdb\-\d{1,}\}\/Season\s\d{2,})$").unwrap());
+static ENCODE_TV_SERIES_DIR_REG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r".+\/(.+\s\{tvdb\-\d{1,}\}\/Season\s\d{2,})$").unwrap());
+static ENCODE_MOVIE_DIR_REG: LazyLock<Regex> = LazyLock::new(|| Regex::new(r".+\/(.+\s\{tvdb\-\d{1,}\})$").unwrap());
 
 pub fn get_session_encode_mapping<P: AsRef<Path>>(source: P, verbose: bool) -> Vec<SessionToEncodeDir> {
   let all_entry_types: Vec<EntryType> =
@@ -16,8 +18,8 @@ pub fn get_session_encode_mapping<P: AsRef<Path>>(source: P, verbose: bool) -> V
       .into_iter()
       .filter_map(|de| de.ok())
       .filter_map(|de| {
-        if de.file_type().is_file() && RENAME_FILE_REG.is_match(de.path().to_str().unwrap()){
-          if let Some((_, [session, file, episode, _])) = RENAME_FILE_REG.captures(de.path().to_str().unwrap()).map(|c| c.extract()) {
+        if de.file_type().is_file() && RENAME_TV_SERIES_FILE_REG.is_match(de.path().to_str().unwrap()){
+          if let Some((_, [session, file, episode, _])) = RENAME_TV_SERIES_FILE_REG.captures(de.path().to_str().unwrap()).map(|c| c.extract()) {
             Some(EntryType::new_rename(de.path(), session, episode, file))
           } else {
             None
@@ -29,8 +31,8 @@ pub fn get_session_encode_mapping<P: AsRef<Path>>(source: P, verbose: bool) -> V
               .map(|encode_file_contents| encode_file_contents.trim().to_owned()) // remove newline added by read_to_string
               .and_then(|encode_file_contents| {
                 let encode_dir = Path::new(&encode_file_contents);
-                if encode_dir.is_dir() && ENCODE_DIR_REG.is_match(&encode_file_contents) {
-                  if let Some((_, [season])) = ENCODE_DIR_REG.captures(&encode_file_contents).map(|c| c.extract()) {
+                if encode_dir.is_dir() && ENCODE_TV_SERIES_DIR_REG.is_match(&encode_file_contents) {
+                  if let Some((_, [season])) = ENCODE_TV_SERIES_DIR_REG.captures(&encode_file_contents).map(|c| c.extract()) {
                     Some(EntryType::new_encodes(&encode_file_contents, season, session))
                   } else {
                     None
@@ -118,13 +120,24 @@ mod tests {
     #[test]
     fn tv_series_regex_match() {
       let path = "/Some/Path/Rips/session1/renames/S01E02 - The Unholy Alliance.mkv";
-      assert_eq!(RENAME_FILE_REG.is_match(path), true);
+      assert_eq!(RENAME_TV_SERIES_FILE_REG.is_match(path), true);
 
-      let (considered_str, [session, file, episode, _]) = RENAME_FILE_REG.captures(path).unwrap().extract();
+      let (considered_str, [session, file, episode, _]) = RENAME_TV_SERIES_FILE_REG.captures(path).unwrap().extract();
       assert_eq!(considered_str, "session1/renames/S01E02 - The Unholy Alliance.mkv");
       assert_eq!(session, "session1");
       assert_eq!(file, "S01E02 - The Unholy Alliance.mkv");
       assert_eq!(episode, "S01E02");
+    }
+
+    #[test]
+    fn movie_regex_match() {
+      let path = "/Some/Path/Rips/session3/renames/Return of the Jedi - {tvdb-698}.mkv";
+      assert_eq!(RENAME_MOVIE_FILE_REG.is_match(path), true);
+
+      let (considered_str, [session, file]) = RENAME_MOVIE_FILE_REG.captures(path).unwrap().extract();
+      assert_eq!(considered_str, "session3/renames/Return of the Jedi - {tvdb-698}.mkv");
+      assert_eq!(session, "session3");
+      assert_eq!(file, "Return of the Jedi - {tvdb-698}.mkv");
     }
 
     #[test]
@@ -138,13 +151,24 @@ mod tests {
     }
 
     #[test]
-    fn encode_file_contents_regex_match() {
+    fn encode_file_tv_series_contents_regex_match() {
       let encode_file_contents = "/Some/Path/Encodes/ThunderCats {tvdb-70355}/Season 01";
-      assert_eq!(ENCODE_DIR_REG.is_match(encode_file_contents), true);
+      assert_eq!(ENCODE_TV_SERIES_DIR_REG.is_match(encode_file_contents), true);
 
-      let (considered_str, [encodes_dir]) = ENCODE_DIR_REG.captures(encode_file_contents).unwrap().extract();
+      let (considered_str, [encodes_dir]) = ENCODE_TV_SERIES_DIR_REG.captures(encode_file_contents).unwrap().extract();
 
       assert_eq!(considered_str, "/Some/Path/Encodes/ThunderCats {tvdb-70355}/Season 01");
       assert_eq!(encodes_dir, "ThunderCats {tvdb-70355}/Season 01");
+    }
+
+    #[test]
+    fn encode_file_movie_contents_regex_match() {
+      let encode_file_contents = "/Some/Path/Encodes/Return of the Jedi - {tvdb-698}";
+      assert_eq!(ENCODE_MOVIE_DIR_REG.is_match(encode_file_contents), true);
+
+      let (considered_str, [encodes_dir]) = ENCODE_MOVIE_DIR_REG.captures(encode_file_contents).unwrap().extract();
+
+      assert_eq!(considered_str, "/Some/Path/Encodes/Return of the Jedi - {tvdb-698}");
+      assert_eq!(encodes_dir, "Return of the Jedi - {tvdb-698}");
     }
 }
